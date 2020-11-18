246c37b8bdcece3e300a9d7412a435db811e6635
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-11-18 17:29:33 -0500

    add requirements.txt

--- data.py
+++ data.py
@@ -1,13 +1,17 @@
 import os
 import numpy as np
+from tqdm import tqdm
+
+
 import torch
 import torchaudio
 
-def make_feats(x_path):
+def make_feats(x_path, out_path):
     files = os.listdir()
     for f in files:
-        waveform, sample_rate = torchaudio.load(f)
+        waveform, sample_rate = torchaudio.load(os.path.join(x_path, f))
         #Calculate MFCC
         mfcc = torchaudio.transforms.MFCC()(waveform)
         #Calculate delta and double-delta
         deltas = torchaudio.transforms.ComputeDeltas()(waveform)
+        
