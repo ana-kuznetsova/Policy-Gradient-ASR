42d4bebc06474479d320c601a34c030923b70f10
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-11-18 21:06:24 -0500

    data preproc

--- data.py
+++ data.py
@@ -12,6 +12,12 @@ def make_feats(x_path, out_path):
         waveform, sample_rate = torchaudio.load(os.path.join(x_path, f))
         #Calculate MFCC
         mfcc = torchaudio.transforms.MFCC()(waveform)
+        print('mffc shape:', mfcc.shape)
         #Calculate delta and double-delta
         deltas = torchaudio.transforms.ComputeDeltas()(waveform)
-        
+        print('deltas:', deltas.shaoe)
+        res = torch.column_stack((mfcc, deltas))
+        torch.save(res, os.path.join(out_path, f.split('.')[0]+'.pt'))
+
+make_feats('/N/slate/anakuzne/Data/asr/policy_gradient/eu/clips', 
+           '/N/slate/anakuzne/Data/asr/policy_gradient/eu/eu_feats')
