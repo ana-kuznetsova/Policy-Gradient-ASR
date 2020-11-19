2db977392eaea04da53c238164004c45f850f57d
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-11-18 21:28:30 -0500

    add tqdm

--- data.py
+++ data.py
@@ -8,14 +8,12 @@ import torchaudio
 
 def make_feats(x_path, out_path):
     files = os.listdir(x_path)
-    for f in files:
+    for f in tqdm(files):
         waveform, sample_rate = torchaudio.load(os.path.join(x_path, f))
         #Calculate MFCC
         mfcc = torchaudio.transforms.MFCC()(waveform)
-        print('mffc shape:', mfcc.shape)
         #Calculate delta and double-delta
         deltas = torchaudio.transforms.ComputeDeltas()(mfcc)
         ddeltas = torchaudio.transforms.ComputeDeltas()(deltas)
         res = torch.cat((mfcc, deltas, ddeltas), dim=1)
-        print("res:", res.shape)
         torch.save(res, os.path.join(out_path, f.split('.')[0]+'.pt'))
