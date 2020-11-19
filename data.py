7100b641e5c4328b6290d6e2e4ea85675509c3da
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-11-18 21:19:26 -0500

    debug

--- data.py
+++ data.py
@@ -14,8 +14,8 @@ def make_feats(x_path, out_path):
         mfcc = torchaudio.transforms.MFCC()(waveform)
         print('mffc shape:', mfcc.shape)
         #Calculate delta and double-delta
-        #stft = torchaudio.transforms.Spectrogram()(waveform)
-        deltas = torchaudio.transforms.ComputeDeltas()(mfcc)
+        stft = torchaudio.transforms.Spectrogram()(waveform)
+        deltas = torchaudio.transforms.ComputeDeltas()(stft)
         print('deltas:', deltas.shape)
         res = torch.column_stack((mfcc, deltas))
         torch.save(res, os.path.join(out_path, f.split('.')[0]+'.pt'))
