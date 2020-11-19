f0fd1cfc4d97578b9daffb2ade4f731044e0d5e3
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-11-18 22:10:13 -0500

    debug

--- data.py
+++ data.py
@@ -19,4 +19,4 @@ def make_feats(x_path, out_path):
         #Normalize rows
         s = torch.sum(res, dim=2, keepdim=True)
         norm = torch.div(res, s)
-        torch.save(res, os.path.join(out_path, f.split('.')[0]+'.pt'))
+        torch.save(norm, os.path.join(out_path, f.split('.')[0]+'.pt'))
