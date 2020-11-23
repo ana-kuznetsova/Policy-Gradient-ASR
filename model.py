c38891b04b74642cd037e33e089e417d9307d038
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-11-23 16:23:41 -0500

    debug

--- model.py
+++ model.py
@@ -67,5 +67,5 @@ def train(num_epochs=50):
         loader = iter(loader)
         for batch in range(len(loader)):
             x, t = loader.next()
-            print(x.shape, len(t))
+            print(x[0].shape, len(t[0]))
         
