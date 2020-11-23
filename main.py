740d73f55eab1a7ce17c8fcb94f445edced0eccb
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-11-23 15:30:20 -0500

    test data loader

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from model import TrainData
 from data import make_feats
+import torch.utils.data as data
 
 
 
@@ -9,6 +10,7 @@ if __name__ == "__main__":
     cv_dataset = TrainData('/N/slate/anakuzne/Data/asr/policy_gradient/eu/train.tsv',
                             '/N/slate/anakuzne/Data/asr/policy_gradient/eu/clips', make_feats)
     
-    for i in range(5):
-        sample = cv_dataset[i]
-        print("Sample:", sample['aud'].shape, sample['trans'])
+    loader = data.DataLoader(cv_dataset, batch_size=32, shuffle=True)
+    for ins in loader:
+        print(ins["aud"].shape)
+        
