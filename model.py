ea2877a869edc72693b8dad2e3275053ffa1d876
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-12-01 13:22:57 -0500

    debug

--- model.py
+++ model.py
@@ -75,9 +75,7 @@ class Attention(nn.Module):
         temp1 = torch.exp(score)
         temp2 = torch.sum(score, dim=0)
         a_t = temp1/temp2
-        print("temp1:", temp1.shape, "temp2:", temp2.shape, "a_t:", a_t.shape)
         c_t = self.c_t
-        print("c_t", c_t.shape)
         for a in a_t:
             c_t+=a*h_e  
         return c_t
@@ -145,6 +143,7 @@ def train(csv_path, aud_path, alphabet_path,  batch_size=32):
         out_enc, (he, ce) = enc(x)
         print("out enc:", out_enc.shape)
         dec_out = dec(out_enc)
+        print("dec out:", dec_out.shape)
 
     '''
     device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
