665e053ddfd458c8a566b916fe5f2e2975542f94
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-12-01 14:07:30 -0500

    debug

--- model.py
+++ model.py
@@ -104,7 +104,7 @@ class Decoder(nn.Module):
                 self.dec_h, self.dec_c = self.lstm_cell(self.y)
             else:
                 self.dec_h, self.dec_c = self.lstm_cell(self.y, (self.dec_h, self.dec_c))
-            print("dec H", self.dec_h)
+            #print("dec H", self.dec_h)
             c_t = self.attention(hidden, self.dec_h)
             #print("C_t:", c_t)
             combined_output = torch.cat([self.dec_h, c_t], 1)
