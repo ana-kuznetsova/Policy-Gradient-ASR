b48baeeaf35b7971f44b57e8e4dfe1ffacd9512d
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-12-01 23:54:52 -0500

    debug

--- model.py
+++ model.py
@@ -56,7 +56,7 @@ class Encoder(nn.Module):
         
     def forward(self, x, mask):
         #x = torch.nan_to_num(x)
-        temp = torch.isnan(x).flatten()
+        temp = torch.isnan(x).squeeze(0)
         temp = [temp for i in temp if temp]
         print("NAN", len(temp))
         outputs=[]
