c158c05965c0df8dcc8d31e8bd252ac8b688ff66
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-12-04 14:01:47 -0500

    debug

--- model.py
+++ model.py
@@ -239,10 +239,8 @@ def predict(test_path, aud_path, alphabet_path, model_path):
             seq = ''.join([ind2char[ind] for ind in seq])
             seq = collapse_fn(seq)
             pad_ind = int(np.sum(tmask[i]))
-            print(pad_ind)
-            #t = t[:pad_ind]
-            #print("t", t)
-            #target = ''.join([ind2char[ind] for ind in t])
-            #print("Target:", target, "Seq:", seq)
+            t = t[:pad_ind]
+            target = ''.join([ind2char[ind] for ind in t])
+            print("Target:", target, "Seq:", seq)
             #cer, wer = evaluate(target, seq)
             
