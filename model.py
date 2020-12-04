176474d1130181aa5f80ae991855ea559c4706ce
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-12-04 13:28:16 -0500

    debug

--- model.py
+++ model.py
@@ -229,5 +229,6 @@ def predict(test_path, aud_path, alphabet_path, model_path):
         for i, probs in enumerate(preds):
             pad_ind = int(np.sum(fmask[i]))
             probs = probs[:pad_ind,]
+            print(probs)
             seq = ctc_decoder.decode(probs, beam_size=5)
-            print("Seq:", seq)
+            #print("Seq:", seq)
