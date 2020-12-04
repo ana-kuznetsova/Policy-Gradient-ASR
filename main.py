f72dc0eb553b61642ef90117d81a335630f276ba
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-12-04 12:41:40 -0500

    fix args

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ def main(args):
 
 if __name__ == "__main__":
    parser = argparse.ArgumentParser()
-   parser.add_argument('--train_path', type=str, help='Path to train csv', required=True)
-   parser.add_argument('--dev_path', type=str, help="Path to dev csv", required=True)
-   parser.add_argument('--test_path', type=str, help='Path to test csv', required=True)
+   parser.add_argument('--train_path', type=str, help='Path to train csv')
+   parser.add_argument('--dev_path', type=str, help="Path to dev csv")
+   parser.add_argument('--test_path', type=str, help='Path to test csv')
    parser.add_argument('--model_path', type=str, help="Directory where model logs and checkpoints will be saved.", required=True)
    parser.add_argument('--aud_path', type=str, help='Path to audio files', required=True)
    parser.add_argument('--alphabet', type=str, help='Path to alphabet file in .txt format', required=True)
