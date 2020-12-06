03e01221d8fabcfd98bf8ee84aa664f8b7d9360c
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-12-06 09:32:42 -0500

    find maxlen

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 from data import find_maxlen
 
-find_maxlen('/nobackup/anakuzne/data/cv/cv-corpus-5.1-2020-06-22/br/clips/')
+find_maxlen('/nobackup/anakuzne/data/cv/cv-corpus-5.1-2020-06-22/eu/clips/')
