9004614dc3bcea139959256dd4f006990bc256ed
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-12-05 23:46:18 -0500

    add parallel processing

--- model.py
+++ model.py
@@ -142,7 +142,7 @@ def train(train_path, dev_path, aud_path, alphabet_path, model_path, maxlen, max
 
     if torch.cuda.device_count() > 1:
         print("Using", torch.cuda.device_count(), "GPUs...")
-        model = nn.DataParallel(model)
+        model = nn.DataParallel(model, device_ids=[0, 1])
     model = model.to(device)
 
     criterion = nn.CTCLoss(zero_infinity=True)
