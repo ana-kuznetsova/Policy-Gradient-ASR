9442c6aabd4bbb3747abf54fecefe9eb49328021
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-12-09 15:39:13 -0500

    debug

--- model.py
+++ model.py
@@ -166,6 +166,7 @@ class AttnDecoderRNN(nn.Module):
             output_i = torch.cat((embedded, attn_applied_i.squeeze(1)), 1)
             output_i = self.attn_combine(output_i).unsqueeze(0)
             output_i = F.relu(output_i)
+            print(dec_hid.shape, c_i.shape)
             output_i, (dec_hid, c_i) = self.lstm(output_i, (dec_hid, c_i))
             output_i = F.log_softmax(self.out(output_i.squeeze(1)), dim=1)
             print(output_i.shape)
