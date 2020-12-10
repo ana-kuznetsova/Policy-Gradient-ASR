04951bd645bf2bd79546d60b1cd772386b5a3312
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-12-10 12:45:34 -0500

    debug

--- data.py
+++ data.py
@@ -99,8 +99,8 @@ def encode_trans(trans, char2ind, maxlen_t=7):
     '''
     res = np.array([char2ind[char] for char in trans])
     res = np.pad(res, (0, maxlen_t-len(res)), 'constant', constant_values=(0))
-    print(res, len(res))
     mask = [1 if i>=0 else 0 for i in res]
+    print(mask)
     return torch.tensor(res), torch.tensor(mask) 
 
 def pad(arr, maxlen):
