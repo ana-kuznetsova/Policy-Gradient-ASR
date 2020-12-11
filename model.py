276cc3f8bfe19ee9f7dd59c24c4791f6d5368578
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-12-10 23:57:28 -0500

    test sampling

--- model.py
+++ model.py
@@ -134,7 +134,7 @@ def train(train_path, dev_path, aud_path, alphabet_path, model_path, maxlen, max
             tmask = batch['tmask'].squeeze(1).to(device)
     
             model_out = model(x, fmask)
-            sampled_t = sample_trans(model_out)
+            sampled_t = sample_trans(model_out, alphabet)
             print(sampled_t)
             '''
             input_lengths = torch.sum(fmask, dim=1).long()
