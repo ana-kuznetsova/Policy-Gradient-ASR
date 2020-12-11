44dd111887dd37d8792c13deaf869e8886b70c40
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-12-11 14:14:03 -0500

    predict

--- model.py
+++ model.py
@@ -271,7 +271,7 @@ def predict(test_path, aud_path, alphabet_path, model_path, batch_size, maxlen, 
         tmask = batch['tmask'].squeeze(1).numpy()
         fmask = batch['fmask'].squeeze(1).to(device)
         dec_input = torch.randn(x.shape[0], 128, requires_grad=True).to(device)
-        preds = model(x, fmask, dec_input)
+        preds = model(x, t, fmask, device)
         preds = torch.transpose(preds, 0, 1).detach().cpu().numpy()
         fmask = fmask.detach().cpu().numpy()
         
