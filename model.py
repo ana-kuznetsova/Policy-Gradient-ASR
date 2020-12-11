d246b7e2ae774f08bc9fb384e140d0815061aaa1
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-12-10 23:12:14 -0500

    debug

--- model.py
+++ model.py
@@ -103,8 +103,6 @@ def train(train_path, dev_path, aud_path, alphabet_path, model_path, maxlen, max
         print("Loaded weights from pretrained...")
         model = Encoder(256, len(alphabet))
         model.load_state_dict(torch.load(os.path.join(model_path, "model_best.pth")))
-        for param in model.parameters():
-            print(param.data)
         
     else:
         model = Encoder(256, len(alphabet))
