472d3740d777e44d5237f9fd1218dff5dfe4d883
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-02-26 17:51:18 -0500

    debug

--- model.py
+++ model.py
@@ -196,8 +196,9 @@ def train(corpus_path, model_path, num_epochs, batch_size, device):
 
     char2ind = {alphabet[i].replace('\n', ''):i for i in range(len(alphabet))}
 
-    device = torch.device("cuda:"+str(device) if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda:0")
     model = Seq2Seq(alphabet_size=len(char2ind))
+    model = nn.DataParallel(actor, device_ids=[0, 3])
     model.apply(weights)
 
     model = model.to(device)
