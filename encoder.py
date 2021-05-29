0a5a65002132affa9b2a2b58e54acaeee13e2b29
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-05-29 16:12:38 -0400

    test encoder

--- encoder.py
+++ encoder.py
@@ -75,6 +75,4 @@ encoder.to(device)
 for batch in loader_train:
     x = batch["feat"].to(device)
     xlens = batch['alens']
-    print("In shape:", x.shape)
     out = encoder(x, xlens)
-    print("Out_shape:", out.shape)
