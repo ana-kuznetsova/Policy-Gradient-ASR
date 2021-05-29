bbfd124ad0fbdacf125b0921b804c15bf9764a59
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-05-29 16:04:36 -0400

    add hi subsampling

--- encoder.py
+++ encoder.py
@@ -2,10 +2,11 @@ import os
 import pandas as pd
 import copy
 import torch
-from torch._C import device
+
 import torch.nn as nn
 import torch.optim as optim
 import torch.utils.data as data
+from torch import utils
 import torchaudio
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 import torch.nn.functional as F
@@ -13,43 +14,49 @@ import torch.nn.functional as F
 from data import preproc_text, Data, collate_custom
 
 
-class Encoder(nn.Module):
-    def __init__(self, feat_dim, inp_size, hid_size):
-        super().__init__()
-        self.input_layer = nn.Linear(feat_dim, inp_size)
-        self.layer_norm = nn.LayerNorm(feat_dim, inp_size)
-        self.blstm1 = nn.LSTM(input_size=inp_size, 
-                             hidden_size=hid_size, 
-                             num_layers=1,
-                             bidirectional=True,
-                             batch_first=True)
-        ## Out from blstm1 is 512
-        self.blstm2 = nn.LSTM(input_size=inp_size, 
-                             hidden_size=hid_size//4, 
-                             num_layers=1,
-                             bidirectional=True,
-                             batch_first=True)
-        ## Out from blstm2 is 512//4 = 128
-        self.blstm3 = nn.LSTM(input_size=inp_size//4, 
-                             hidden_size=hid_size//8, 
-                             num_layers=1,
-                             bidirectional=True,
-                             batch_first=True)
-        
-    def forward(self, x, lens):
-        x = torch.transpose(x, 1, -1)
-        x = self.layer_norm(x)
-        print("Lnorm:", x.shape)
-        x = F.leaky_relu(self.input_layer(x))
-        print("Inp L", x.shape)
-        x = pack_padded_sequence(x, lens, enforce_sorted=False, batch_first=True)
-        x, _ = self.blstm1(x)
-        x, _ = self.blstm2(x)
-        x, _ = self.blstm3(x)
-        output, _ = pad_packed_sequence(x, batch_first=True)
-        print("Final blstm:", output.shape)
-        return output
+class pBLSTM(nn.Module):
+    def __init__(self, input_dim, hidden_dim):
+        super(pBLSTM, self).__init__()
+        self.blstm = nn.LSTM(input_size=input_dim,hidden_size=hidden_dim,num_layers=1,bidirectional=True)
+    def forward(self,x):
+        return self.blstm(x)
 
+
+class Encoder(nn.Module):
+    def __init__(self, input_dim, hidden_dim, value_size=128,key_size=128):
+        super(Encoder, self).__init__()
+        self.lstm = nn.LSTM(input_size=input_dim,hidden_size=hidden_dim,num_layers=1,bidirectional=True)
+        self.pBLSTM1= pBLSTM(2*hidden_dim, hidden_dim)
+        self.pBLSTM2= pBLSTM(2*hidden_dim, hidden_dim)
+        self.pBLSTM3= pBLSTM(2*hidden_dim, hidden_dim)
+        self.key_network = nn.Linear(hidden_dim*2, value_size)
+        self.value_network = nn.Linear(hidden_dim*2, key_size)
+
+    def forward(self, x, lens):
+        rnn_inp = pack_padded_sequence(x, lengths=lens, enforce_sorted=False)
+        outputs, _ = self.lstm(rnn_inp)
+        linear_input, _ = pad_packed_sequence(outputs)
+        
+        for i in range(3):
+            if linear_input.shape[0]%2!=0:
+                linear_input = linear_input[:-1,:,:]
+            outputs = torch.transpose(linear_input, 0, 1)
+            outputs = outputs.contiguous().view(outputs.shape[0], outputs.shape[1]//2, 2, outputs.shape[2])
+            outputs = torch.mean(outputs, 2)
+            outputs = torch.transpose(outputs,0,1)
+            lens=lens//2
+            rnn_inp = utils.rnn.pack_padded_sequence(outputs, lengths=lens, enforce_sorted=False)
+            if i==0:
+                outputs, _ = self.pBLSTM1(rnn_inp)
+            elif i==1:
+                outputs, _ = self.pBLSTM2(rnn_inp)
+            else:
+                outputs, _ = self.pBLSTM3(rnn_inp)
+            linear_input, _ = pad_packed_sequence(outputs)
+        keys = self.key_network(linear_input)
+        value = self.value_network(linear_input)
+
+        return keys, value, lens
 
 corpus_path = '/nobackup/anakuzne/data/cv/cv-corpus-6.1-2020-12-11/eu'
 char2ind = preproc_text(corpus_path, 'eu')
@@ -60,7 +67,7 @@ loader_train = data.DataLoader(dataset_train, batch_size=5,
 
 device ="cuda:1"
 
-encoder = Encoder(120, 512, 256)
+encoder = Encoder(120, 256)
 encoder.to(device)
 
 for batch in loader_train:
