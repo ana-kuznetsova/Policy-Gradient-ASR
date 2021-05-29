69b2d5e9616c0c704a4707f6f9471c46db398138
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-05-29 16:53:36 -0400

    debug decoder

--- attn_decoder.py
+++ attn_decoder.py
@@ -57,6 +57,7 @@ class Decoder(nn.Module):
         batch_size=key.shape[1]
         if(train):
             text=torch.transpose(text,0,1)
+            print(text.shape)
             max_len=text.shape[1]
             embeddings=self.embedding(text)
         else:
